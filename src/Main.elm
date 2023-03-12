2c62058e060a15ce53959563ba6bb4f845bb97e5
Thomas Heinemann <data1701-e@web.de>
2023-03-12 22:44:01 +0100

    Add feedback message after each answer

--- src/Main.elm
+++ src/Main.elm
@@ -17,17 +17,27 @@ main =
 
 -- MODEL
 
+type Answer = Correct | Wrong Int
+
+isCorrect : Answer -> Bool
+isCorrect answer = 
+    case answer of
+        Correct -> True
+        _ -> False
+
 type alias Model = {
         currentValue: Maybe Int,
         remaining: List Int,
-        answered: List (Int, Bool)
+        answered: List (Answer),
+        previous: Maybe Answer
     }
 
 init : Model
 init = { 
     currentValue = Nothing,
     remaining = List.range 1 20,
-    answered = []
+    answered = [],
+    previous = Nothing
  }
 
 
@@ -44,30 +54,37 @@ update msg model =
     Submit ->
       let currentNumber = List.head model.remaining |> Maybe.withDefault 0
           expected = currentNumber * 2
-          correct = (Maybe.withDefault -1 model.currentValue) == expected
-          updatedAnswers = (currentNumber, correct) :: model.answered
+          answer = if (Maybe.withDefault -1 model.currentValue) == expected then Correct else Wrong currentNumber
+          updatedAnswers = answer :: model.answered
        in {
               currentValue = Nothing,
               remaining = List.tail model.remaining |> Maybe.withDefault [],
-              answered = updatedAnswers
+              answered = updatedAnswers,
+              previous = Just (answer)
           }
 
 
 -- VIEW
 
+answerMessage answer =
+    case answer of
+        Correct -> div [] [ text "Richtig!" ]
+        Wrong n -> div [] [ text ("Leider Falsch. Das doppelte von " ++ (String.fromInt n) ++ " ist " ++ (String.fromInt (n * 2)) ++ "!") ]
+
 view : Model -> Html Msg
 view model =
-  case List.head model.remaining of
-    Just currentNumber ->
-        div []
-                [ div [] [ text ("Was ist das doppelte von " ++ String.fromInt currentNumber)]
-                , input [ onInput Change, type_ "number", value ( (Maybe.map String.fromInt model.currentValue) |> Maybe.withDefault "" ) ] []
-                , button [ onClick Submit ] [ text "Ok" ]
-                ]
-    Nothing -> 
-        let numbers = List.length model.answered
-            correctAnswers = List.length ( List.filter Tuple.second model.answered)
-         in div []
-                [ div [] [ text "Game over.\n" ]
-                , div [] [ text ("Du hast " ++ (String.fromInt correctAnswers) ++ " von " ++ (String.fromInt numbers)  ++ " Aufgaben korrekt gelöst!") ]
-                ]
+  let feedback = Maybe.map (\answer -> [ answerMessage answer ]) model.previous |> Maybe.withDefault []
+   in case List.head model.remaining of
+        Just currentNumber ->
+            div []
+                    (feedback ++ [ div [] [ text ("Was ist das doppelte von " ++ String.fromInt currentNumber)]
+                    , input [ onInput Change, type_ "number", value ( (Maybe.map String.fromInt model.currentValue) |> Maybe.withDefault "" ) ] []
+                    , button [ onClick Submit ] [ text "Ok" ]
+                    ])
+        Nothing -> 
+            let numbers = List.length model.answered
+                correctAnswers = List.length ( List.filter isCorrect model.answered)
+            in div []
+                    (feedback ++ [ div [] [ text "Game over.\n" ]
+                    , div [] [ text ("Du hast " ++ (String.fromInt correctAnswers) ++ " von " ++ (String.fromInt numbers)  ++ " Aufgaben korrekt gelöst!") ]
+                    ])
