5c9ee2b72cbee4097fb5b3522c0f20829a9d454f
Thomas Heinemann <data1701-e@web.de>
2023-03-14 18:50:01 +0100

    Refactoring: Explicitly model initialisation

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
 import Browser
-import Html.Styled exposing (Html, div, text, input, br, h1, toUnstyled)
+import Html.Styled exposing (Html, div, text, input, br, h1, h2, toUnstyled)
 import Html.Styled.Events exposing (onSubmit, onInput)
 import Html.Styled.Attributes exposing (type_, value, class, autofocus, css)
 import Random.List
@@ -15,8 +15,6 @@ import Css exposing (..)
 main =
   Browser.element { init = init, update = updateWrapper, view = view >> toUnstyled, subscriptions = (\_ -> Sub.none) }
 
-
-
 -- MODEL
 
 type Answer = Correct | Wrong Int
@@ -27,20 +25,17 @@ isCorrect answer =
         Correct -> True
         _ -> False
 
-type alias Model = {
+type alias GameState = {
         currentValue: Maybe Int,
         remaining: List Int,
         answered: List (Answer),
         previous: Maybe Answer
     }
 
+type Model = Initialising | InGame GameState | Error String
+
 init : () -> (Model, Cmd Msg)
-init _ = ({ 
-    currentValue = Nothing,
-    remaining = [],
-    answered = [],
-    previous = Nothing
- }, Random.generate Input (Random.List.shuffle (List.range 1 20)) )
+init _ = (Initialising, Random.generate Input (Random.List.shuffle (List.range 1 20)))
 
 
 -- UPDATE
@@ -50,22 +45,29 @@ type Msg = Change String | Submit | Input (List Int)
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Change val ->
-      { model | currentValue = String.toInt val }
-    Submit ->
-      let currentNumber = List.head model.remaining |> Maybe.withDefault 0
+  case (msg, model) of
+    (Change val, InGame gameState) ->
+      InGame { gameState | currentValue = String.toInt val }
+    (Submit, InGame gameState) ->
+      let currentNumber = List.head gameState.remaining |> Maybe.withDefault 0
           expected = currentNumber * 2
-          answer = if (Maybe.withDefault -1 model.currentValue) == expected then Correct else Wrong currentNumber
-          updatedAnswers = answer :: model.answered
-       in {
+          answer = if (Maybe.withDefault -1 gameState.currentValue) == expected then Correct else Wrong currentNumber
+          updatedAnswers = answer :: gameState.answered
+       in InGame {
               currentValue = Nothing,
-              remaining = List.tail model.remaining |> Maybe.withDefault [],
+              remaining = List.tail gameState.remaining |> Maybe.withDefault [],
               answered = updatedAnswers,
               previous = Just (answer)
           }
-    Input list ->
-      { model | remaining = list }
+    (Input list, _) ->
+      InGame { 
+            currentValue = Nothing,
+            remaining = list,
+            answered = [],
+            previous = Nothing
+        }
+    _ ->  -- We should never end up here 
+      Error "Invalid combination of message and model. This should never happen."
 
 
 updateWrapper : Msg -> Model -> (Model, Cmd Msg)
@@ -87,27 +89,32 @@ view model =
     , div [] [view1 model]
     ]
 
+defaultMargin : List Style
 defaultMargin = [ margin (px 3) ]
 
 view1 : Model -> Html Msg
 view1 model =
-  let feedback = Maybe.map (\answer -> [ answerMessage answer ]) model.previous |> Maybe.withDefault []
-   in case List.head model.remaining of
-        Just currentNumber ->
-            div []
-                    [ Keyed.node "form" [ onSubmit Submit ]
-                        ((List.map (\x -> ("feedback", x)) feedback) ++ 
-                          [ ("question", div [] [ text ("Was ist das Doppelte von " ++ String.fromInt currentNumber ++ "?")])
-                          , ("input", input [ onInput Change, type_ "number", value ( (Maybe.map String.fromInt model.currentValue) |> Maybe.withDefault "" ), css defaultMargin, autofocus True ] [])
-                          , ("br", br [] [])
-                          , ("submitButton", input [ type_ "submit", css defaultMargin ] [ text "Ok" ])
-                          ]
-                        )
-                    ]
-        Nothing -> 
-            let numbers = List.length model.answered
-                correctAnswers = List.length ( List.filter isCorrect model.answered)
-            in div []
-                    (feedback ++ [ div [] [ text "Game over.\n" ]
-                    , div [] [ text ("Du hast " ++ (String.fromInt correctAnswers) ++ " von " ++ (String.fromInt numbers)  ++ " Aufgaben korrekt gelöst!") ]
-                    ])
+  case model of
+    InGame gameState ->
+      let feedback = Maybe.map (\answer -> [ answerMessage answer ]) gameState.previous |> Maybe.withDefault []
+      in case List.head gameState.remaining of
+            Just currentNumber ->
+                div []
+                        [ Keyed.node "form" [ onSubmit Submit ]
+                            ((List.map (\x -> ("feedback", x)) feedback) ++ 
+                              [ ("question", div [] [ text ("Was ist das Doppelte von " ++ String.fromInt currentNumber ++ "?")])
+                              , ("input", input [ onInput Change, type_ "number", value ( (Maybe.map String.fromInt gameState.currentValue) |> Maybe.withDefault "" ), css defaultMargin, autofocus True ] [])
+                              , ("br", br [] [])
+                              , ("submitButton", input [ type_ "submit", css defaultMargin ] [ text "Ok" ])
+                              ]
+                            )
+                        ]
+            Nothing -> 
+                let numbers = List.length gameState.answered
+                    correctAnswers = List.length ( List.filter isCorrect gameState.answered)
+                in div []
+                        (feedback ++ [ div [] [ text "Game over.\n" ]
+                        , div [] [ text ("Du hast " ++ (String.fromInt correctAnswers) ++ " von " ++ (String.fromInt numbers)  ++ " Aufgaben korrekt gelöst!") ]
+                        ])
+    Error msg -> div [] [ text ("Interner Fehler: " ++ msg ++ ". Dies ist ein Bug.") ]
+    _ -> h2 [] [text "Laden. Bitte warten..."]
