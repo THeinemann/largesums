932b149b764839d5eb55879b3f426a5e2e3ddcd6
Thomas Heinemann <data1701-e@web.de>
2024-11-16 16:30:23 +0100

    Fix test

--- tests/MainTest.elm
+++ tests/MainTest.elm
@@ -1,22 +1,26 @@
 module MainTest exposing (..)
 
 import Expect
-import Test exposing (..)
 import Main
+import Test exposing (..)
 
 
 expectFalse : Bool -> Expect.Expectation
-expectFalse = Expect.equal False
+expectFalse =
+    Expect.equal False
+
 
 initTests : Test
 initTests =
     describe "Initialisation" <|
-        let (actualMessage, actualCommand) = Main.init()
-         in [ test "Should start with correct initial command" (
-                    (\_ -> Expect.equal (actualCommand) Main.shuffleCommand)
-                )
-            , test "Should start with correct initial state" <|
-                \_ ->
-                    Main.initialised actualMessage
-                        |> expectFalse
-            ]
+        let
+            ( actualMessage, actualCommand ) =
+                Main.init ()
+        in
+        [ test "Should start with correct initial command"
+            (\_ -> Expect.equal actualCommand Main.buildTasks)
+        , test "Should start with correct initial state" <|
+            \_ ->
+                Main.initialised actualMessage
+                    |> expectFalse
+        ]
